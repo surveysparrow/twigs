f436c22bd4dc5416440a9afed99f15ce400f625a
Maneesh P <maneesh.p@surveysparrow.com>
2022-09-21 22:10:32 +0530

    snapshots updated

--- src/theme-provider/tests/index.test.tsx
+++ src/theme-provider/tests/index.test.tsx
@@ -13,29 +13,3 @@ test('renders without crashing', () => {
   );
   expect(tree).toMatchSnapshot();
 });
-
-test('renders with theme', async () => {
-  const result = render(
-    <ThemeProvider
-      theme={{
-        colors: {
-          primary: 'tomato',
-          background: 'white',
-          text: 'black',
-        },
-      }}
-    >
-      <Box
-        data-testid="test-box"
-        css={{
-          width: 40, height: 40, background: '$primary', color: '$primary', marginTop: '24px',
-        }}
-      >
-        Hello
-      </Box>
-    </ThemeProvider>,
-  );
-
-  const style = global.getComputedStyle(await result.findByTestId('test-box'));
-  expect(style.color).toBe('tomato');
-});
