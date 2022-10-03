8b26406becd5924c5f975b520cca6318ac6a3278
Justin George <justin.george@surveysparrow.com>
2022-10-03 15:09:40 +0530

    default value for avatar

--- src/avatar/stories/avatar.stories.tsx
+++ src/avatar/stories/avatar.stories.tsx
@@ -16,6 +16,7 @@ export default {
     },
     src: {
       control: 'text',
+      defaultValue: 'https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80',
     },
   },
 } as ComponentMeta<typeof Avatar>;
