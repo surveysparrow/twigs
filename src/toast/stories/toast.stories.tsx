6dfc020f81acae1c1d4c9fe2a5b6680e5df75e19
Maneesh P <maneesh.p@surveysparrow.com>
2022-10-25 17:45:18 +0530

    added toast component

---
+++ src/toast/stories/toast.stories.tsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Button } from '../../button';
+import { Box } from '../../box';
+import { Flex } from '../../flex';
+import {
+  ToastProvider, Toast, ToastDescription, ToastAction, ToastContent
+} from '../toast';
+
+export default {
+  component: Toast,
+  title: 'Toast',
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['success', 'error', 'warning', 'info'],
+      defaultValue: 'success'
+    }
+  }
+} as ComponentMeta<typeof Toast>;
+
+const Template: ComponentStory<typeof Toast> = (args) => {
+  const [open, setOpen] = useState(false);
+  const [variant, setVariant] = useState<any>('success');
+  const timerRef = useRef(0);
+
+  useEffect(() => {
+    return () => clearTimeout(timerRef.current);
+  }, []);
+  return (
+    <ToastProvider position="bottom-center">
+      <Flex css={{ gap: '$3' }}>
+        <Button
+          size="lg"
+          onClick={() => {
+            setOpen(false);
+            setVariant('success');
+            window.clearTimeout(timerRef.current);
+            timerRef.current = window.setTimeout(() => {
+              setOpen(true);
+            }, 100);
+          }}
+        >
+          Success Toast
+        </Button>
+        <Button
+          size="lg"
+          onClick={() => {
+            setOpen(false);
+            setVariant('info');
+            window.clearTimeout(timerRef.current);
+            timerRef.current = window.setTimeout(() => {
+              setOpen(true);
+            }, 100);
+          }}
+        >
+          Info Toast
+        </Button>
+        <Button
+          size="lg"
+          onClick={() => {
+            setOpen(false);
+            setVariant('error');
+            window.clearTimeout(timerRef.current);
+            timerRef.current = window.setTimeout(() => {
+              setOpen(true);
+            }, 100);
+          }}
+        >
+          Error Toast
+        </Button>
+        <Button
+          size="lg"
+          onClick={() => {
+            setOpen(false);
+            setVariant('warning');
+            window.clearTimeout(timerRef.current);
+            timerRef.current = window.setTimeout(() => {
+              setOpen(true);
+            }, 100);
+          }}
+        >
+          Warning Toast
+        </Button>
+      </Flex>
+
+      <Toast open={open} size="md" onOpenChange={setOpen} {...args} variant={variant}>
+        <ToastContent>
+          <ToastDescription>Toast Message description</ToastDescription>
+        </ToastContent>
+        <ToastAction asChild altText="Goto schedule to undo">
+          <Box>
+            <Button variant="primary" isText size="md">
+              Ok
+            </Button>
+            <Button variant="accent" isText isTransparent size="md">
+              Cancel
+            </Button>
+          </Box>
+        </ToastAction>
+      </Toast>
+    </ToastProvider>
+  );
+};
+export const Default = Template.bind({});
