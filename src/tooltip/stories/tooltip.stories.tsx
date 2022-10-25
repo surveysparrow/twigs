8c1c04a5ba9c28c2de5b9a57b936010e7c97878b
Maneesh P <maneesh.p@surveysparrow.com>
2022-10-25 17:45:39 +0530

    added tooltip component

---
+++ src/tooltip/stories/tooltip.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Button } from '../../button';
+import {
+  Provider, Tooltip
+} from '../tooltip';
+import { Flex } from '../../flex';
+
+export default {
+  component: Tooltip,
+  title: 'Tooltip',
+  argTypes: {
+    side: {
+      control: 'select',
+      options: ['bottom', 'top', 'left', 'right'],
+      defaultValue: 'top'
+    },
+    align: {
+      control: 'select',
+      options: ['start', 'center', 'end']
+    },
+    sideOffset: {
+      control: 'number'
+    }
+  }
+} as ComponentMeta<typeof Tooltip>;
+
+const Template: ComponentStory<typeof Tooltip> = (args) => (
+  <Provider delayDuration={0}>
+    <Flex
+      css={{
+        height: '100vh',
+        width: '100vw',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Tooltip
+        side="top"
+        align="end"
+        {...args}
+        content="Tooltip content is very large asfas asfasf asfasfasf asfasfasfaf asfasf"
+      >
+        <Button variant="default" size="lg">
+          tooltip button
+        </Button>
+      </Tooltip>
+    </Flex>
+  </Provider>
+);
+export const Default = Template.bind({});
---
+++ src/tooltip/tests/index.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  render, screen, fireEvent, waitFor
+} from '@testing-library/react';
+import 'jest';
+import '@testing-library/jest-dom';
+import { Button } from '../../button';
+import { Flex } from '../../flex';
+import { Provider, Tooltip } from '../tooltip';
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn()
+}));
+
+const Component = () => {
+  return (
+    <Provider delayDuration={0}>
+      <Flex
+        css={{
+          height: '100vh',
+          width: '100vw',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Tooltip
+          open
+          side="top"
+          align="end"
+          content="Tooltip content"
+        >
+          <Button variant="default" size="lg">
+            tooltip button
+          </Button>
+        </Tooltip>
+      </Flex>
+    </Provider>
+  );
+};
+
+test('renders tooltip on hover of button', async () => {
+  render(<Component />);
+  const button = screen.getByRole('button');
+  fireEvent.mouseOver(button);
+  await waitFor(() => {
+    expect(screen.getByRole('tooltip')).toBeInTheDocument();
+  });
+});
