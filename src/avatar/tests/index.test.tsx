a070fbd1e54cd2ac143106e8bb079c0668cabc14
Justin George <justin.george@surveysparrow.com>
2022-11-01 14:44:36 +0530

    updated avatar props and tests

--- src/avatar/tests/index.test.tsx
+++ src/avatar/tests/index.test.tsx
@@ -5,10 +5,15 @@ import type { RenderResult } from '@testing-library/react';
 import { createRoot } from 'react-dom/client';
 import { act } from 'react-test-renderer';
 import { render } from '@testing-library/react';
-import { Avatar, AvatarImage, AvatarFallback } from '../avatar';
+import { Avatar } from '../avatar';
 
-const ROOT_TEST_ID = 'avatar-root';
-const FALLBACK_TEXT = 'AB';
+const getFallbackInitials = (name: string): string => {
+  const [firstName, lastName] = name.split(' ');
+  return `${firstName ? firstName.charAt(0).toUpperCase() : ''}${lastName ? lastName.charAt(0).toUpperCase() : ''}`;
+};
+
+const FALLBACK_TEXT = 'Adam Smith';
+const FALLBACK_INITIALS = getFallbackInitials(FALLBACK_TEXT);
 const IMAGE_ALT_TEXT = 'Fake Avatar';
 const DELAY = 300;
 
@@ -18,13 +23,11 @@ describe('Avatar', () => {
     const root = createRoot(div!);
     act(() => {
       root.render(
-        <Avatar>
-          <AvatarImage
-            src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
-            alt="Pedro Duarte"
-          />
-          <AvatarFallback delayMs={DELAY}>{FALLBACK_TEXT}</AvatarFallback>
-        </Avatar>,
+        <Avatar
+          src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
+          name="Pedro Duarte"
+          fallbackDelay={DELAY}
+        />
       );
       root.unmount();
     });
@@ -37,19 +40,11 @@ describe('given an Avatar with fallback and a working image', () => {
 
   beforeEach(() => {
     rendered = render(
-      <Avatar>
-        <AvatarImage
-          src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-          alt={IMAGE_ALT_TEXT}
-        />
-        <AvatarFallback>{FALLBACK_TEXT}</AvatarFallback>
-      </Avatar>,
+      <Avatar
+        src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+        name={IMAGE_ALT_TEXT}
+      />
     );
-  });
-
-  it('should render the fallback initially', () => {
-    const fallback = rendered.queryByText(FALLBACK_TEXT);
-    expect(fallback).toBeInTheDocument();
   });
 
   it('should not render the image initially', () => {
@@ -64,9 +59,11 @@ describe('given an Avatar with fallback and delayed render', () => {
 
   beforeEach(() => {
     rendered = render(
-      <Avatar data-testid={ROOT_TEST_ID}>
-        <AvatarFallback delayMs={DELAY}>{FALLBACK_TEXT}</AvatarFallback>
-      </Avatar>,
+      <Avatar
+        src="https://invalid-image.png"
+        name={FALLBACK_TEXT}
+        fallbackDelay={DELAY}
+      />
     );
   });
 
@@ -76,9 +73,9 @@ describe('given an Avatar with fallback and delayed render', () => {
   });
 
   it('should render a fallback after the delay', async () => {
-    fallback = rendered.queryByText(FALLBACK_TEXT);
+    fallback = rendered.queryByText(FALLBACK_INITIALS);
     expect(fallback).not.toBeInTheDocument();
-    fallback = await rendered.findByText(FALLBACK_TEXT);
+    fallback = await rendered.findByText(FALLBACK_INITIALS);
     expect(fallback).toBeInTheDocument();
   });
 });
