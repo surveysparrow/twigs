7376835cfd03749c7e77138778c92499a7cfb025
Justin George <justin.george@surveysparrow.com>
2022-11-15 18:04:21 +0530

    removed live-server and react-test-renderer

--- packages/react-components/src/grid/tests/index.test.tsx
+++ packages/react-components/src/grid/tests/index.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'jest';
 import '@testing-library/jest-dom';
-import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import { Grid } from '../index';
 
@@ -12,70 +11,5 @@ describe('Grid', () => {
 
   it('renders with width prop', () => {
     render(<Grid width={350} templateColumns="1fr 1fr 1fr" />);
-  });
-
-  it('matches snapshot: renders with templatearea props', () => {
-    const tree = renderer.create(
-      <Grid
-        templateAreas={`"navbar navbar"
-                        "sidebar main"`}
-      />
-    );
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('matches snapshot: renders with templatearea and templateColumns props', () => {
-    const tree = renderer.create(<Grid
-      templateAreas={`"navbar navbar"
-                        "sidebar main"`}
-      templateColumns="342px 1fr"
-    />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('matches snapshot: renders with templatearea and templateRows props', () => {
-    const tree = renderer.create(<Grid
-      templateAreas={`"navbar navbar"
-        "sidebar main"`}
-      templateRows="64px 1fr"
-    />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('matches snapshot: renders with templatearea, templateRows and templateColumns props', () => {
-    const tree = renderer.create(<Grid
-      templateAreas={`"navbar navbar"
-          "sidebar main"`}
-      templateRows="64px 1fr"
-      templateColumns="342px 1fr"
-    />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('matches snapshot: renders with templatearea, templateColumns props and repeat props', () => {
-    const tree = renderer.create(<Grid
-      templateAreas={`"navbar navbar"
-                        "sidebar main"`}
-      templateColumns="342px 1fr"
-      repeat="fit"
-    />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('matches snapshot: renders with templatearea, templateRows, templateColumns, gap and custom styles', () => {
-    const tree = renderer.create(
-      <Grid
-        templateAreas={`"navbar navbar"
-                         "sidebar main"`}
-        templateRows="64px 1fr"
-        templateColumns="342px 1fr"
-        gap={[0, 0]}
-        css={{
-          height: '100vh',
-          width: '100vw'
-        }}
-      />
-    );
-    expect(tree).toMatchSnapshot();
   });
 });
