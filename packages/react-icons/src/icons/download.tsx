604dcd6e85fcf8cf96a0e192d2017dbe235e1209
Justin George <justin.george@surveysparrow.com>
2022-12-09 15:37:48 +0530

    download icon format

--- packages/react-icons/src/icons/download.tsx
+++ packages/react-icons/src/icons/download.tsx
@@ -5,7 +5,9 @@ export const DownloadIcon = React.forwardRef<SVGSVGElement, IconProps>(
   ({ color = 'currentColor', size = 32, ...rest }, ref) => {
     return (
       <svg
-        viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 32" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
         {...rest}
         width={size}
         height={size}
