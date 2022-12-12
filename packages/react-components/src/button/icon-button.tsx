37581bd9ec0dfb8a756bfc11d47b08dda4ec717a
Justin George <justin.george@surveysparrow.com>
2022-12-12 17:35:08 +0530

    updated icon button props

--- packages/react-components/src/button/icon-button.tsx
+++ packages/react-components/src/button/icon-button.tsx
@@ -1,20 +1,15 @@
-import React, { FunctionComponent, isValidElement, ReactElement } from 'react';
+import React, { FunctionComponent, isValidElement, ComponentProps, ReactElement } from 'react';
 import { Button, ButtonBaseProps } from './button';
 
 type OmitProps = 'iconLeft' | 'iconRight' | 'isText' | 'isIcon';
 
 export interface IconButtonBaseProps {
   icon: ReactElement,
-  'aria-label': string,
-  variant: 'default' | 'primary' | 'accent' | 'secondary',
-  size: 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl',
-  isTransparent: boolean
+  'aria-label': string
 }
 
-interface BaseButtonProps extends Omit<ButtonBaseProps, OmitProps> { }
-type IconButtonProps = IconButtonBaseProps &
-  BaseButtonProps &
-  React.ButtonHTMLAttributes<HTMLButtonElement>;
+interface BaseButtonProps extends ComponentProps<typeof Button> { }
+type IconButtonProps = IconButtonBaseProps & Omit<BaseButtonProps, OmitProps>;
 
 // @TODO: revisit the ref approach here
 export const IconButton: FunctionComponent<IconButtonProps> = React.forwardRef(
@@ -22,8 +17,7 @@ export const IconButton: FunctionComponent<IconButtonProps> = React.forwardRef(
     {
       children, icon, 'aria-label': ariaLabel, ...rest
     }: IconButtonProps,
-    // eslint-disable-next-line
-    ref 
+    ref
   ) => {
     const element = icon || children;
     const validElement = isValidElement(element) ? element : null;
@@ -31,6 +25,7 @@ export const IconButton: FunctionComponent<IconButtonProps> = React.forwardRef(
       <Button
         aria-label={ariaLabel}
         icon={validElement}
+        ref={ref}
         {...rest}
       />
     );
