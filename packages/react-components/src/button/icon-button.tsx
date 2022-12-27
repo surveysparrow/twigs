4f2f8f43f0d85b6639819b04d09537b9a925ec51
Justin George <justin.george@surveysparrow.com>
2022-12-27 16:26:03 +0530

    corrected icon button type

--- packages/react-components/src/button/icon-button.tsx
+++ packages/react-components/src/button/icon-button.tsx
@@ -1,16 +1,14 @@
-import React, { FunctionComponent, isValidElement, ReactElement } from 'react';
+import React, { ComponentProps, FunctionComponent, isValidElement, ReactElement } from 'react';
 import { Button, ButtonBaseProps } from './button';
 
-type OmitProps = 'iconLeft' | 'iconRight' | 'isTransparent';
+type OmitProps = 'iconLeft' | 'iconRight' | 'isIcon' | 'isText';
 
 export interface IconButtonBaseProps {
   icon: ReactElement,
   'aria-label': string
 }
 
-interface BaseButtonProps extends Omit<ButtonBaseProps, OmitProps> { }
-type IconButtonProps = IconButtonBaseProps &
-  BaseButtonProps &
+type IconButtonProps = IconButtonBaseProps & Omit<ComponentProps<typeof Button>, OmitProps> &
   React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 // @TODO: revisit the ref approach here
@@ -19,13 +17,13 @@ export const IconButton: FunctionComponent<IconButtonProps> = React.forwardRef(
     {
       children, icon, 'aria-label': ariaLabel, ...rest
     }: IconButtonProps,
-    // eslint-disable-next-line
-    ref 
+    ref
   ) => {
     const element = icon || children;
     const validElement = isValidElement(element) ? element : null;
     return (
       <Button
+        ref={ref}
         aria-label={ariaLabel}
         icon={validElement}
         {...rest}
