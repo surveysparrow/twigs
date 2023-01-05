5cd884414dbc0bc12c73eb800cb021eacbf12399
Justin George <justin.george@surveysparrow.com>
2023-01-05 14:35:39 +0530

    added avatar badge component

---
+++ packages/react-components/src/avatar/avatar-badge.tsx
@@ -0,0 +1,15 @@
+import { styled } from "../../stitches.config";
+
+export const AvatarBadge = styled('div', {
+  borderRadius: '$round',
+  position: 'absolute',
+  background: '$negative500',
+  border: '$borderWidths$xs',
+  borderStyle: 'solid',
+  borderColor: '$white900',
+  right: 0,
+  top: 0,
+  height: '6px',
+  width: '6px',
+  transform: 'translate(25%, -25%)'
+});
