28d9fe015008630c6c226d2d613b36ff891de060
Justin George <justin.george@surveysparrow.com>
2023-01-24 17:42:56 +0530

    added min view for pagination

--- packages/react-components/src/pagination/pagination.tsx
+++ packages/react-components/src/pagination/pagination.tsx
@@ -1,7 +1,7 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@sparrowengg/twigs-react-icons';
 import React, { SyntheticEvent, useState } from 'react';
 import { styled } from "../../stitches.config";
-import { Button } from '../button';
+import { Button, IconButton } from '../button';
 
 const StyledContainer = styled('nav', {
   display: 'flex',
@@ -10,27 +10,16 @@ const StyledContainer = styled('nav', {
 });
 
 const StyledPage = styled(Button, {
-  fontWeight: '$7',
-  fontSize: '$sm',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  cursor: 'pointer',
-  height: '$8',
-  width: '$7',
-  outline: 'none',
-  border: 'none',
-  borderRadius: '$md',
-  backgroundColor: 'transparent',
+  backgroundColor: 'transparent !important',
   '&:focus': {
-    border: '1px',
-    borderStyle: 'solid',
-    borderColor: '$neutral100'
+    border: '1px !important',
+    borderStyle: 'solid !important',
+    borderColor: '$neutral100 !important'
   },
   variants: {
     active: {
       true: {
-        backgroundColor: '$neutral100'
+        backgroundColor: '$neutral100 !important'
       }
     },
     disabled: {
@@ -50,17 +39,32 @@ export type IProps = {
   onChange?: (event, page: number) => void
 }
 
-const calcPaginationRange = ({ total, itemsPerPage, maxItems, activePage }) => {
+const calcPaginationRange = ({ total, itemsPerPage, activePage }) => {
+
+  // maximum items we can show as pages
+  const maxItems = 5;
   const items = Math.floor(total / itemsPerPage);
 
   const hasEllipsis = total > maxItems;
 
-  const startEllipsis = hasEllipsis && !!(maxItems - activePage < 4) && activePage > 3;
-  const endEllipsis = hasEllipsis && !!(activePage < total - maxItems + 4) && items - activePage > 2
+  // enable ellipsis after 1, if current page is further away from maxItems - 4, and active page should be greater than 3.
+  // Ideally we want to avoid showing ellipsis when user is on page 1, 2
+  const startEllipsis = hasEllipsis && !!(maxItems - 4 < activePage) && activePage > 3;
 
-  const sliceStart = startEllipsis ? activePage - 2 : 1;
-  const sliceEnd = endEllipsis ? activePage + 1 : items - 1;
+  // show end ellipsis if the current page is further than total - max + 4 from the last page and active page > 2
+  const endEllipsis = hasEllipsis && !!(activePage < total - maxItems + 4) && items - activePage > 2;
 
+  // after the ellipsis, where to start with the page number
+  let sliceStart = startEllipsis ? activePage - 2 : 1;
+  // where to end before the next ellipsis
+  let sliceEnd = endEllipsis ? activePage + 1 : items - 1;
+  // below logic is to keep min of 4 buttons in the UI, otherwise, pagination component becomes too small
+  if (sliceEnd !== items - 1 && sliceEnd - sliceStart < 2) {
+    sliceEnd++;
+  }
+  if (sliceStart > 1 && sliceEnd - sliceStart < 2) {
+    sliceStart--;
+  }
   return {
     items, startEllipsis, endEllipsis, sliceStart, sliceEnd
   }
@@ -78,8 +82,7 @@ export const Pagination = React.forwardRef(
   }: IProps, ref) => {
 
     const [page, setPage] = useState(activePage);
-    const maxItems = 5;
-    const { items, startEllipsis, endEllipsis, sliceStart, sliceEnd } = calcPaginationRange({ total, itemsPerPage, maxItems, activePage: page });
+    const { items, startEllipsis, endEllipsis, sliceStart, sliceEnd } = calcPaginationRange({ total, itemsPerPage, activePage: page });
 
     const changeActivePage = (event: SyntheticEvent, page: number) => {
       setPage(page);
@@ -92,20 +95,20 @@ export const Pagination = React.forwardRef(
       <StyledContainer
         aria-label='pagination'
       >
-        <StyledPage
+        <IconButton
+          icon={<ChevronLeftIcon />}
           disabled={page === 1}
           aria-disabled={page === 1}
-        >
-          <ChevronLeftIcon
-            aria-label='Previous'
-            onClick={(event: SyntheticEvent) => changeActivePage(event, page - 1)}
-          />
-        </StyledPage>
+          aria-label='Previous'
+          variant={'default'}
+          onClick={(event: SyntheticEvent) => changeActivePage(event, page - 1)}
+        />
 
         <StyledPage
           active={page === 1}
           onClick={(event: SyntheticEvent) => changeActivePage(event, page - 1)}
           aria-label={page === 1 ? 'page' : ''}
+          variant={'default'}
         > 1 </StyledPage>
 
         {
@@ -122,6 +125,7 @@ export const Pagination = React.forwardRef(
               <StyledPage
                 onClick={(event: SyntheticEvent) => changeActivePage(event, pageNo)}
                 active={page === pageNo}
+                variant={'default'}
                 aria-label={page === pageNo ? 'page' : ''}
               > {pageNo} </StyledPage>
             )
@@ -139,19 +143,19 @@ export const Pagination = React.forwardRef(
               active={page === items}
               onClick={(event: SyntheticEvent) => changeActivePage(event, items)}
               aria-label={page === items ? 'page' : ''}
+              variant={'default'}
             > {items} </StyledPage>
             :
             null
         }
-        <StyledPage
+        <IconButton
+          icon={<ChevronRightIcon />}
+          aria-label='Next'
+          variant={'default'}
           disabled={page === items}
           aria-disabled={page === items}
           onClick={(event: SyntheticEvent) => changeActivePage(event, page + 1)}
-        >
-          <ChevronRightIcon
-            aria-label='Next'
-          />
-        </StyledPage>
+        />
       </StyledContainer>
     )
   }
