2151de5a6050d9460b1ec1d4d5825ae49fbb9dae
Justin George <justin.george@surveysparrow.com>
2023-01-27 14:59:32 +0530

    updated pagination logic and a11y

--- packages/react-components/src/pagination/pagination.tsx
+++ packages/react-components/src/pagination/pagination.tsx
@@ -1,74 +1,25 @@
-import { ChevronLeftIcon, ChevronRightIcon } from '@sparrowengg/twigs-react-icons';
-import React, { SyntheticEvent, useState } from 'react';
+import { ChevronLeftIcon, ChevronRightIcon, EllipsisHorizontalIcon } from '@sparrowengg/twigs-react-icons';
+import React, { FunctionComponent, SyntheticEvent, useId, useState } from 'react';
 import { styled } from "../../stitches.config";
+import { Box, BoxProps } from '../box';
 import { Button, IconButton } from '../button';
+import { usePagination } from './use-pagination';
 
-const StyledContainer = styled('nav', {
+const StyledList = styled('ul', {
   display: 'flex',
   alignItems: 'center',
-  gap: '$4'
+  gap: '$2'
 });
 
-const StyledPage = styled(Button, {
-  backgroundColor: 'transparent !important',
-  '&:focus': {
-    border: '1px !important',
-    borderStyle: 'solid !important',
-    borderColor: '$neutral100 !important'
-  },
-  variants: {
-    active: {
-      true: {
-        backgroundColor: '$neutral100 !important'
-      }
-    },
-    disabled: {
-      true: {
-        cursor: 'not-allowed',
-        color: '$neutral300',
-      }
-    },
-
-  }
-});
-
-export type IProps = {
+export type PaginationProps = {
   total: number,
   itemsPerPage: number,
   activePage: number,
+  siblingCount?: number,
   onChange?: (event, page: number) => void
-}
+} & BoxProps;
 
-const calcPaginationRange = ({ total, itemsPerPage, activePage }) => {
-
-  // maximum items we can show as pages
-  const maxItems = 5;
-  const items = Math.floor(total / itemsPerPage);
-
-  const hasEllipsis = total > maxItems;
-
-  // enable ellipsis after 1, if current page is further away from maxItems - 4, and active page should be greater than 3.
-  // Ideally we want to avoid showing ellipsis when user is on page 1, 2
-  const startEllipsis = hasEllipsis && !!(maxItems - 4 < activePage) && activePage > 3;
-
-  // show end ellipsis if the current page is further than total - max + 4 from the last page and active page > 2
-  const endEllipsis = hasEllipsis && !!(activePage < total - maxItems + 4) && items - activePage > 2;
-
-  // after the ellipsis, where to start with the page number
-  let sliceStart = startEllipsis ? activePage - 2 : 1;
-  // where to end before the next ellipsis
-  let sliceEnd = endEllipsis ? activePage + 1 : items - 1;
-  // below logic is to keep min of 4 buttons in the UI, otherwise, pagination component becomes too small
-  if (sliceEnd !== items - 1 && sliceEnd - sliceStart < 2) {
-    sliceEnd++;
-  }
-  if (sliceStart > 1 && sliceEnd - sliceStart < 2) {
-    sliceStart--;
-  }
-  return {
-    items, startEllipsis, endEllipsis, sliceStart, sliceEnd
-  }
-}
+const DOTS = 'DOTS';
 
 /**
  * @param total total items inside pagination
@@ -76,13 +27,17 @@ const calcPaginationRange = ({ total, itemsPerPage, activePage }) => {
  * @param activePage currently focused page
  * @param onChange returns click event and active page number
  */
-export const Pagination = React.forwardRef(
+export const Pagination: FunctionComponent<PaginationProps> = React.forwardRef(
   ({
-    total, itemsPerPage, activePage, onChange
-  }: IProps, ref) => {
+    total, itemsPerPage, activePage, siblingCount, onChange, ...rest
+  }: PaginationProps, ref) => {
 
-    const [page, setPage] = useState(activePage);
-    const { items, startEllipsis, endEllipsis, sliceStart, sliceEnd } = calcPaginationRange({ total, itemsPerPage, activePage: page });
+    const keyId = useId();
+    const [page, setPage] = useState<number>(activePage);
+
+    const paginationRange = usePagination({
+      totalItems: total, itemsPerPage, activePage: page, siblingCount
+    });
 
     const changeActivePage = (event: SyntheticEvent, page: number) => {
       setPage(page);
@@ -92,71 +47,72 @@ export const Pagination = React.forwardRef(
     }
 
     return (
-      <StyledContainer
+      <Box
+        ref={ref}
+        as={'nav'}
         aria-label='pagination'
+        {...rest}
       >
-        <IconButton
-          icon={<ChevronLeftIcon />}
-          disabled={page === 1}
-          aria-disabled={page === 1}
-          aria-label='Previous'
-          variant={'default'}
-          onClick={(event: SyntheticEvent) => changeActivePage(event, page - 1)}
-        />
-
-        <StyledPage
-          active={page === 1}
-          onClick={(event: SyntheticEvent) => changeActivePage(event, page - 1)}
-          aria-label={page === 1 ? 'page' : ''}
-          variant={'default'}
-        > 1 </StyledPage>
-
-        {
-          startEllipsis ?
-            '...'
-            :
-            null
-        }
-
-        {
-          [...Array(items).keys()].slice(sliceStart, sliceEnd).map(item => {
-            const pageNo = item + 1;
-            return (
-              <StyledPage
-                onClick={(event: SyntheticEvent) => changeActivePage(event, pageNo)}
-                active={page === pageNo}
-                variant={'default'}
-                aria-label={page === pageNo ? 'page' : ''}
-              > {pageNo} </StyledPage>
-            )
-          })
-        }
-        {
-          endEllipsis ?
-            '...'
-            :
-            null
-        }
-        {
-          items > 1 ?
-            <StyledPage
-              active={page === items}
-              onClick={(event: SyntheticEvent) => changeActivePage(event, items)}
-              aria-label={page === items ? 'page' : ''}
-              variant={'default'}
-            > {items} </StyledPage>
-            :
-            null
-        }
-        <IconButton
-          icon={<ChevronRightIcon />}
-          aria-label='Next'
-          variant={'default'}
-          disabled={page === items}
-          aria-disabled={page === items}
-          onClick={(event: SyntheticEvent) => changeActivePage(event, page + 1)}
-        />
-      </StyledContainer>
+        <StyledList>
+          <li>
+            <IconButton
+              as='a'
+              icon={<ChevronLeftIcon />}
+              disabled={page === 1}
+              aria-disabled={page === 1}
+              aria-label='Previous'
+              variant={'bright'}
+              role='link'
+              onClick={(event: SyntheticEvent) => changeActivePage(event, page - 1)}
+            />
+          </li>
+          {
+            paginationRange?.map((pageNumber, index) => {
+              if (pageNumber === DOTS) {
+                return (
+                  <li key={`pagination-dots-${keyId}-${index}`}>
+                    <IconButton
+                      as={'a'}
+                      icon={<EllipsisHorizontalIcon />}
+                      role={'presentation'}
+                      variant={'bright'}
+                      css={{
+                        cursor: 'default'
+                      }}
+                    />
+                  </li>
+                )
+              }
+              return (
+                <li key={`pagination-${keyId}-page-${pageNumber}`}>
+                  <Button
+                    as={'a'}
+                    onClick={(event: SyntheticEvent) => changeActivePage(event, Number(pageNumber))}
+                    aria-label={`Page ${pageNumber}`}
+                    variant={page === pageNumber ? 'default' : 'bright'}
+                    aria-current={page === pageNumber}
+                    role='link'
+                  >
+                    {pageNumber}
+                  </Button>
+                </li>
+              )
+            })
+          }
+          <li>
+            <IconButton
+              as='a'
+              icon={<ChevronRightIcon />}
+              aria-label='Next'
+              variant={'bright'}
+              role='link'
+              disabled={page === paginationRange?.[paginationRange?.length - 1]}
+              aria-disabled={page === paginationRange?.[paginationRange?.length - 1]}
+              onClick={(event: SyntheticEvent) => changeActivePage(event, page + 1)}
+            />
+          </li>
+        </StyledList>
+      </Box>
     )
   }
 )
--- packages/react-components/src/pagination/stories/pagination.stories.tsx
+++ packages/react-components/src/pagination/stories/pagination.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Pagination } from '../pagination';
 
 export default {
