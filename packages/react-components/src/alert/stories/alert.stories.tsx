92f944eff31860f82ba434742accf84dfc0baecb
Justin George <justin.george@surveysparrow.com>
2023-01-30 17:25:53 +0530

    alert icon story

---
+++ packages/react-components/src/alert/stories/alert.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Alert, AlertDescription } from '../alert';
+
+export default {
+  component: Alert,
+  title: 'Alert',
+  argTypes: {
+    status: {
+      control: 'select',
+      options: ['success', 'info', 'error', 'warning'],
+      defaultValue: 'info'
+    },
+    size: {
+      control: 'select',
+      options: ['sm', 'md'],
+      defaultValue: 'sm'
+    }
+  }
+} as ComponentMeta<typeof Alert>;
+
+const Template: ComponentStory<typeof Alert> = (args) => (
+  <Alert
+    {...args}
+    css={{
+      maxWidth: 400
+    }}
+  >
+    <AlertDescription>
+      {`A ${args.status} message here`}
+    </AlertDescription>
+  </Alert>
+);
+export const Default = Template.bind({});
