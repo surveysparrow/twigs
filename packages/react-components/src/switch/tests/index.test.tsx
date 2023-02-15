597e39c96f610edad7f15cb8349e75f60c627c9b
Justin George <justin.george@surveysparrow.com>
2023-02-15 15:47:43 +0530

    corrected the tests for api changes

--- packages/react-components/src/switch/tests/index.test.tsx
+++ packages/react-components/src/switch/tests/index.test.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import 'jest';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import { Switch, SwitchThumb } from '../index';
+import { Switch } from '../index';
 
 describe('Switch', () => {
   it('renders without crashing', () => {
     render(
-      <Switch defaultChecked id="s1">
-        <SwitchThumb />
-      </Switch>
+      <Switch defaultChecked id="s1"
+        isDisabled={false} isChecked={false} onChange={() => { }} isRequired={false}
+      />
     );
   });
 });
