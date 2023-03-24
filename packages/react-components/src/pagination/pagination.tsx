606761e58d82e7cea86d3ca3b8967f530d8146b5
Justin George <justin.george@surveysparrow.com>
2023-03-24 11:16:22 +0530

    removed unnecessary ref

--- packages/react-components/src/pagination/pagination.tsx
+++ packages/react-components/src/pagination/pagination.tsx
@@ -30,101 +30,99 @@ const DOTS = 'DOTS';
  * @param activePage currently focused page
  * @param onChange returns click event and active page number
  */
-export const Pagination: FunctionComponent<PaginationProps> = React.forwardRef(
-  ({
-    total, itemsPerPage, activePage, siblingCount, onChange, ...rest
-  }: PaginationProps, ref) => {
-    const keyId = useId();
-    const [page, setPage] = useState<number>(activePage);
+export const Pagination: FunctionComponent<PaginationProps> = ({
+  total, itemsPerPage, activePage, siblingCount, onChange, ...rest
+}: PaginationProps) => {
+  const keyId = useId();
+  const [page, setPage] = useState<number>(activePage);
 
-    const paginationRange = usePagination({
-      totalItems: total, itemsPerPage, activePage: page, siblingCount
-    });
+  const paginationRange = usePagination({
+    totalItems: total, itemsPerPage, activePage: page, siblingCount
+  });
 
-    const changeActivePage = (event: SyntheticEvent, newPage: number) => {
-      setPage(newPage);
-      if (onChange) {
-        onChange(event, newPage);
-      }
-    };
+  const changeActivePage = (event: SyntheticEvent, newPage: number) => {
+    setPage(newPage);
+    if (onChange) {
+      onChange(event, newPage);
+    }
+  };
 
-    const isPrevDisabled = page === 1;
-    const isNextDisabled = page === paginationRange?.[paginationRange?.length - 1];
+  const isPrevDisabled = page === 1;
+  const isNextDisabled = page === paginationRange?.[paginationRange?.length - 1];
 
-    return (
-      <Box
-        ref={ref}
-        as="nav"
-        aria-label="pagination"
-        {...rest}
-      >
-        <StyledList>
-          <li>
-            <IconButton
-              as="a"
-              icon={<ChevronLeftIcon />}
-              css={{
-                cursor: isPrevDisabled ? 'not-allowed' : 'pointer',
-                opacity: isPrevDisabled ? 0.4 : 1
-              }}
-              aria-disabled={isPrevDisabled}
-              aria-label="Previous"
-              variant="bright"
-              role="link"
-              onClick={(event: SyntheticEvent) => changeActivePage(event, page - 1)}
-            />
-          </li>
-          {
-            paginationRange?.map((pageNumber, index) => {
-              if (pageNumber === DOTS) {
-                return (
-                  // eslint-disable-next-line react/no-array-index-key
-                  <li key={`pagination-dots-${keyId}-${index}`}>
-                    <IconButton
-                      as="a"
-                      icon={<EllipsisHorizontalIcon />}
-                      role="presentation"
-                      variant="bright"
-                      css={{
-                        cursor: 'default'
-                      }}
-                    />
-                  </li>
-                );
-              }
+  return (
+    <Box
+      as="nav"
+      aria-label="pagination"
+      {...rest}
+    >
+      <StyledList>
+        <li>
+          <IconButton
+            as="a"
+            icon={<ChevronLeftIcon />}
+            css={{
+              cursor: isPrevDisabled ? 'not-allowed' : 'pointer',
+              opacity: isPrevDisabled ? 0.4 : 1
+            }}
+            aria-disabled={isPrevDisabled}
+            aria-label="Previous"
+            variant="bright"
+            role="link"
+            onClick={(event: SyntheticEvent) => changeActivePage(event, page - 1)}
+          />
+        </li>
+        {
+          paginationRange?.map((pageNumber, index) => {
+            if (pageNumber === DOTS) {
               return (
-                <li key={`pagination-${keyId}-page-${pageNumber}`}>
-                  <Button
+                // eslint-disable-next-line react/no-array-index-key
+                <li key={`pagination-dots-${keyId}-${index}`}>
+                  <IconButton
                     as="a"
-                    onClick={(event: SyntheticEvent) => changeActivePage(event, Number(pageNumber))}
-                    aria-label={`Page ${pageNumber}`}
-                    variant={page === pageNumber ? 'default' : 'bright'}
-                    aria-current={page === pageNumber}
-                    role="link"
-                  >
-                    {pageNumber}
-                  </Button>
+                    icon={<EllipsisHorizontalIcon />}
+                    role="presentation"
+                    variant="bright"
+                    css={{
+                      cursor: 'default'
+                    }}
+                  />
                 </li>
               );
-            })
-          }
-          <li>
-            <IconButton
-              as="a"
-              icon={<ChevronRightIcon />}
-              aria-label="Next"
-              variant="bright"
-              role="link"
-              css={{
-                cursor: isNextDisabled ? 'not-allowed' : 'pointer',
-                opacity: isNextDisabled ? 0.4 : 1
-              }}
-              aria-disabled={isNextDisabled}
-              onClick={(event: SyntheticEvent) => changeActivePage(event, page + 1)}
-            />
-          </li>
-        </StyledList>
-      </Box>
-    );
-  }
-);
+            }
+            return (
+              <li key={`pagination-${keyId}-page-${pageNumber}`}>
+                <Button
+                  as="a"
+                  onClick={(event: SyntheticEvent) => changeActivePage(event, Number(pageNumber))}
+                  aria-label={`Page ${pageNumber}`}
+                  variant={page === pageNumber ? 'default' : 'bright'}
+                  aria-current={page === pageNumber}
+                  role="link"
+                >
+                  {pageNumber}
+                </Button>
+              </li>
+            );
+          })
+        }
+        <li>
+          <IconButton
+            as="a"
+            icon={<ChevronRightIcon />}
+            aria-label="Next"
+            variant="bright"
+            role="link"
+            css={{
+              cursor: isNextDisabled ? 'not-allowed' : 'pointer',
+              opacity: isNextDisabled ? 0.4 : 1
+            }}
+            isDisabled={isNextDisabled}
+            aria-disabled={isNextDisabled}
+            onClick={(event: SyntheticEvent) => changeActivePage(event, page + 1)}
+          />
+        </li>
+      </StyledList>
+    </Box>
+  );
+};
