2a1ef49a36f8656ba712392d7a3303c9b21b0a84
meghan87 <115980448+meghan87@users.noreply.github.com>
2023-04-23 12:34:34 +0530

    infoplus icon size issue fixed

--- packages/react-icons/src/icons/info-plus.tsx
+++ packages/react-icons/src/icons/info-plus.tsx
@@ -13,19 +13,12 @@ export const InfoPlusIcon = React.forwardRef<SVGSVGElement, IconProps>(
         height={size}
         ref={ref}
       >
-        <g clipPath="url(#clip0_8237_6018)">
-          <path d="M14.4861 3.28397C13.8474 3.13903 13.1826 3.0625 12.5 3.0625C7.56395 3.0625 3.5625 7.06395 3.5625 12C3.5625 16.936 7.56395 20.9375 12.5 20.9375C17.436 20.9375 21.4375 16.936 21.4375 12C21.4375 11.2759 21.3514 10.5719 21.1888 9.89767" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-          <path d="M11.6875 15.25H13.3125" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-          <path d="M12.5 15.25V11.1875H11.6875" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-          <ellipse cx="12.5" cy="8.75" rx="0.8125" ry="0.8125" fill={color} />
-          <path d="M19.75 2.25V6.25" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-          <path d="M17.75 4.25H21.75" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-        </g>
-        <defs>
-          <clipPath id="clip0_8237_6018">
-            <rect width={size} height={size} fill="white" transform="translate(0.5)" />
-          </clipPath>
-        </defs>
+        <path d="M14.4861 3.28397C13.8474 3.13903 13.1826 3.0625 12.5 3.0625C7.56395 3.0625 3.5625 7.06395 3.5625 12C3.5625 16.936 7.56395 20.9375 12.5 20.9375C17.436 20.9375 21.4375 16.936 21.4375 12C21.4375 11.2759 21.3514 10.5719 21.1888 9.89767" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M11.6875 15.25H13.3125" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M12.5 15.25V11.1875H11.6875" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+        <ellipse cx="12.5" cy="8.75" rx="0.8125" ry="0.8125" fill={color} />
+        <path d="M19.75 2.25V6.25" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M17.75 4.25H21.75" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
     );
   }
