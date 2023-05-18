c36d3cfec3a8cff537bc9e9c9fd2212d6720ed5a
Justin George <justin.george@surveysparrow.com>
2023-05-18 15:36:12 +0530

    corrected exports

--- packages/react-components/src/toast/toastr.tsx
+++ packages/react-components/src/toast/toastr.tsx
@@ -8,7 +8,7 @@ import {
   ToastTitle,
   ToastViewport
 } from './toast';
-import { useToast } from '../hooks/use-toast';
+import { useToast } from '../hooks';
 
 export const Toaster = () => {
   const { toasts } = useToast();
