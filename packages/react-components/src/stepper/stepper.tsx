5b3da8e0f21fcf47de1766e255ced51d68c556b1
Justin George <justin.george@surveysparrow.com>
2023-09-01 10:14:23 +0530

    ignore stepper changes

--- packages/react-components/src/stepper/stepper.tsx
+++ packages/react-components/src/stepper/stepper.tsx
@@ -9,11 +9,11 @@ export type StepperProps = {
   activeStep: number,
   children: React.ReactElement[],
   // eslint-disable-next-line no-unused-vars
-  onChange: (step: number) => void,
+  onChange?: (step: number) => void,
   components?: {
     TriggerSeparator?: React.ComponentType
     TriggerContainer?: React.ComponentType<{ children: React.ReactNode }>,
-    TriggerButton?: React.ComponentType<{ index: number, children: React.ReactNode, active?: boolean, completed?: boolean }>,
+    TriggerButton?: React.ComponentType<{ children: React.ReactNode, active?: boolean, completed?: boolean }>,
   }
 } & BoxProps;
 
@@ -54,6 +54,7 @@ const Stepper: FunctionComponent<StepperProps> = forwardRef(({
   const Separator = components.TriggerSeparator || DefaultSeparator;
   const TriggerContainer = components.TriggerContainer || DefaultStepperTrigger.StepperTriggerContainer;
   const TriggerButton = components.TriggerButton || DefaultStepperTrigger.StepperTrigger;
+
   return (
     <StyledStepper ref={ref}>
       <TriggerContainer>
@@ -66,18 +67,15 @@ const Stepper: FunctionComponent<StepperProps> = forwardRef(({
             return (
               <React.Fragment key={`stepper-control-${stepperId}-${id}`}>
                 <TriggerButton
-                  // tabIndex={allowClick ? 0 : -1}
-                  title="my-react-button"
-                  index={index}
+                  tabIndex={allowClick ? 0 : -1}
                   active={index === activeStep}
                   completed={completed}
-                  // type="button"
+                  type="button"
                   cursor={allowClick ? 'pointer' : 'default'}
                   {...(onChange && allowClick && {
                     onClick: () => onChange(index)
                   })}
                   {...rest}
-                  onClick={() => console.log(index)}
                 >
                   <DefaultStepperTrigger.StepperCount>
                     {
