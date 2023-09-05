583d79bf7987f8f28b56c376efc2094af0d7d4d7
Akshay U <akshay.uthaman@surveysparrow.com>
2023-09-05 14:41:25 +0530

    Update button test case to handle loader and icon props

--- packages/react-components/src/button/tests/index.test.tsx
+++ packages/react-components/src/button/tests/index.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'jest';
 import { render } from '@testing-library/react';
+import { TickCircleFillIcon } from '@sparrowengg/twigs-react-icons';
 import '@testing-library/jest-dom';
 import { Button } from '../index';
 
@@ -16,5 +17,40 @@ describe('Button', () => {
     const button = getByTestId('button');
 
     expect(button).toHaveTextContent('Primary Button');
+  });
+
+  it('renders button with left icon', () => {
+    const { getByTestId } = render(
+      <Button color="primary" leftIcon={<TickCircleFillIcon />}>
+        Primary Button
+      </Button>
+    );
+    const button = getByTestId('button');
+    const iconContainer = button.querySelector('.button-icon-container');
+
+    expect(iconContainer).toBeDefined();
+  });
+
+  it('renders button with loader', () => {
+    const { getByTestId, rerender } = render(
+      <Button color="primary" leftIcon={<TickCircleFillIcon />}>
+        Primary Button
+      </Button>
+    );
+    const button = getByTestId('button');
+
+    expect(
+      button.querySelector('.button-side-element-enter-done')
+    ).toBeDefined();
+
+    rerender(
+      <Button color="primary" leftIcon={<TickCircleFillIcon />} loading>
+        Primary Button
+      </Button>
+    );
+
+    expect(
+      button.querySelector('.button-side-element-exit-done')
+    ).toBeDefined();
   });
 });
