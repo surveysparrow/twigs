caefea7c79dd884cebbe74fe91db9f5a3339a3f2
Akshay U <akshay.uthaman@surveysparrow.com>
2023-09-05 14:38:41 +0530

    Add basic testing for line and circle loader

--- packages/react-components/src/loader/tests/index.test.tsx
+++ packages/react-components/src/loader/tests/index.test.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { DotLoader } from '../index';
+import { CircleLoader, DotLoader, LineLoader } from '../index';
 import 'jest';
 import '@testing-library/jest-dom';
 
-describe('Box', () => {
+describe('Dot Loader', () => {
   it('renders without crashing', () => {
     render(<DotLoader />);
+  });
+});
+
+describe('Circle Loader', () => {
+  it('renders without crashing', () => {
+    render(<CircleLoader />);
+  });
+});
+
+describe('Line Loader', () => {
+  it('renders without crashing', () => {
+    render(<LineLoader />);
   });
 });
