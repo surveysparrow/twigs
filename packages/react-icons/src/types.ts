7c1fdb8ef8a5b2bcec5a04cc29b6f124172c078a
meghan87 <meghanath.shetty@surveysparrow.com>
2023-10-04 16:47:10 +0530

    added space

--- packages/react-icons/src/types.ts
+++ packages/react-icons/src/types.ts
@@ -3,5 +3,5 @@ import React from 'react';
 export interface IconProps extends React.SVGAttributes<SVGElement> {
   color?: string,
   size?: number,
-  strokeWidth?:number;
+  strokeWidth?: number
 }
