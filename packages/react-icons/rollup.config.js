d3844fb7eaba78c1ba4d0d11472a1caf85281a68
Akshay U <akshay.uthaman@surveysparrow.com>
2023-10-05 18:44:41 +0530

    Use same rollup config as that of components for icons, fix paths in package json. Remove unnecessary files

--- packages/react-icons/rollup.config.js
+++ packages/react-icons/rollup.config.js
@@ -1,9 +1,10 @@
 const resolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
-const typescript = require('@rollup/plugin-typescript');
 const external = require('rollup-plugin-peer-deps-external');
 const postcss = require('rollup-plugin-postcss');
 const { PluginPure } = require('rollup-plugin-pure');
+const { dts } = require('rollup-plugin-dts');
+const esbuild = require('rollup-plugin-esbuild').default;
 
 const packageJson = require('./package.json');
 
@@ -18,27 +19,54 @@ module.exports = [
      */
     output: [
       {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true
-      },
-      {
-        file: packageJson.module,
+        dir: 'dist/es',
         format: 'es',
         sourcemap: true,
-        esModule: true
+        esModule: true,
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        exports: 'named'
+      },
+      {
+        dir: 'dist/cjs',
+        format: 'cjs',
+        sourcemap: true,
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        exports: 'named'
       }
     ],
-    external: [Object.keys(packageJson.peerDependencies)],
+    external: [...Object.keys(packageJson.peerDependencies)],
     plugins: [
       external(),
       PluginPure({
-        functions: ['React.forwardRef']
+        functions: ['forwardRef', 'React.forwardRef', 'styled']
       }),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.prod.json' }),
+      esbuild({
+        include: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        sourceMap: true,
+        target: 'es2017',
+        jsx: 'transform',
+        jsxFactory: 'React.createElement',
+        jsxFragment: 'React.Fragment',
+        tsconfig: 'tsconfig.prod.json',
+        loaders: {
+          '.json': 'json',
+          '.js': 'jsx'
+        }
+      }),
       postcss()
     ]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es'
+    },
+    plugins: [dts()]
   }
 ];
