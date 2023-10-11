4b9220984e8f1832c4e4d319f9dac2396aa1d564
meghan87 <meghanath.shetty@surveysparrow.com>
2023-10-11 11:49:36 +0530

    required indicator added for both form input and label

--- packages/react-components/src/form-label/form-label.tsx
+++ packages/react-components/src/form-label/form-label.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { FunctionComponent, ReactElement } from 'react';
 import * as LabelPrimitive from '@radix-ui/react-label';
 import { styled } from '../../stitches.config';
+import { Flex } from '../flex';
+import { Text } from '../text';
 
 export type FormLabelProps = React.HTMLAttributes<HTMLLabelElement> & {
-  as?: React.ElementType
+  as?: React.ElementType;
+  requiredIndicator?: ReactElement | boolean;
 };
 
-export const FormLabel = styled(LabelPrimitive.Root, {
+const StyledFormLabel = styled(LabelPrimitive.Root, {
   display: 'block',
   color: '$neutral800',
   fontWeight: '$4',
@@ -27,3 +30,18 @@ export const FormLabel = styled(LabelPrimitive.Root, {
     size: 'sm'
   }
 });
+
+export const FormLabel: FunctionComponent<FormLabelProps> = ({
+  children,
+  requiredIndicator = false,
+  as
+}: FormLabelProps) => {
+  return (
+    <Flex gap="$1">
+      <StyledFormLabel as={as}>{children}</StyledFormLabel>
+      {requiredIndicator === true ? <Text css={{ color: 'red', paddingTop: '$1' }}>*</Text> : null}
+      {typeof requiredIndicator !== 'boolean'
+        ? React.cloneElement(requiredIndicator) : null}
+    </Flex>
+  );
+};
