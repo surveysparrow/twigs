4e693190c4e138559f88fdeafb1a24d1b4581df2
Akshay U <akshay.uthaman@surveysparrow.com>
2023-11-16 16:46:59 +0530

    Update dialog tests to include more cases

--- packages/react-components/src/dialog/tests/dialogs-manager.test.tsx
+++ packages/react-components/src/dialog/tests/dialogs-manager.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'jest';
 import '@testing-library/jest-dom';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { DialogsManager, dialogs } from '../dialogs-manager';
 
 describe('Checkbox', () => {
@@ -35,8 +35,10 @@ describe('Checkbox', () => {
       </div>
     );
     fireEvent.click(getByText('Trigger'));
-    const dialogElements = getAllByRole('alertdialog');
-    expect(dialogElements.length).toBe(1);
+    const dialogElements = getAllByRole('alertdialog', {
+      hidden: true
+    });
+    expect(dialogElements).toHaveLength(1);
   });
 
   it('opens alert dialog', () => {
@@ -47,7 +49,10 @@ describe('Checkbox', () => {
           onClick={() => {
             dialogs.open('alert', {
               title: 'Alert',
-              content: 'Content'
+              content: 'Content',
+              labels: {
+                action: 'Okay'
+              }
             });
           }}
         >
@@ -60,5 +65,158 @@ describe('Checkbox', () => {
     const dialogElements = getByRole('alertdialog');
     expect(dialogElements).toBeInTheDocument();
     expect(getByText('Alert')).toBeInTheDocument();
+    expect(getByText('Content')).toBeInTheDocument();
+    expect(getByText('Okay')).toBeInTheDocument();
+  });
+
+  it('opens confirm dialog', () => {
+    const { getByText, getByRole } = render(
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            dialogs.open('confirm', {
+              title: 'Title',
+              content: 'Content',
+              labels: {
+                confirm: 'Okay',
+                cancel: 'Cancel'
+              }
+            });
+          }}
+        >
+          Trigger
+        </button>
+        <DialogsManager />
+      </div>
+    );
+    fireEvent.click(getByText('Trigger'));
+    const dialogElements = getByRole('alertdialog');
+    expect(dialogElements).toBeInTheDocument();
+    expect(getByText('Title')).toBeInTheDocument();
+    expect(getByText('Content')).toBeInTheDocument();
+    expect(getByText('Okay')).toBeInTheDocument();
+    expect(getByText('Cancel')).toBeInTheDocument();
+  });
+
+  it('opens modal dialog', () => {
+    const { getByText, getByRole } = render(
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            dialogs.open('modal', {
+              title: 'Title',
+              content: 'Content',
+              labels: {
+                action: 'Action'
+              }
+            });
+          }}
+        >
+          Trigger
+        </button>
+        <DialogsManager />
+      </div>
+    );
+    fireEvent.click(getByText('Trigger'));
+    const dialogElements = getByRole('dialog');
+    expect(dialogElements).toBeInTheDocument();
+    expect(getByText('Title')).toBeInTheDocument();
+    expect(getByText('Content')).toBeInTheDocument();
+    expect(getByText('Action')).toBeInTheDocument();
+  });
+
+  it('opens and closes alert dialog', () => {
+    const { getByText, getByRole, queryByRole } = render(
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            const { close: closeDialog } = dialogs.open('alert', {
+              title: 'Alert',
+              content: (
+                <div>
+                  <button type="button" onClick={() => closeDialog()}>
+                    Close
+                  </button>
+                </div>
+              ),
+              labels: {
+                action: 'Okay'
+              }
+            });
+          }}
+        >
+          Trigger
+        </button>
+        <DialogsManager />
+      </div>
+    );
+    fireEvent.click(getByText('Trigger'));
+    const dialogElements = getByRole('alertdialog');
+    expect(dialogElements).toBeInTheDocument();
+    fireEvent.click(getByText('Close'));
+    expect(queryByRole('alertdialog')).not.toBeInTheDocument();
+  });
+
+  it('opens multiple alert dialogs and closes them all', async () => {
+    const {
+      getByText, queryByRole, queryAllByRole
+    } = render(
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            dialogs.push('alert', {
+              title: 'Alert',
+              content: (
+                <div>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      dialogs.push('alert', {
+                        title: 'Second Alert',
+                        content: (
+                          <div>
+                            <button
+                              type="button"
+                              onClick={() => dialogs.closeAll()}
+                            >
+                              Close All
+                            </button>
+                          </div>
+                        ),
+                        labels: {
+                          action: 'Okay'
+                        }
+                      });
+                    }}
+                  >
+                    Second Trigger
+                  </button>
+                </div>
+              ),
+              labels: {
+                action: 'Okay'
+              }
+            });
+          }}
+        >
+          Trigger
+        </button>
+        <DialogsManager />
+      </div>
+    );
+    fireEvent.click(getByText('Trigger'));
+    fireEvent.click(getByText('Second Trigger'));
+
+    await waitFor(() => expect(getByText('Close All')).toBeInTheDocument());
+    const dialogElements = queryAllByRole('alertdialog', {
+      hidden: true
+    });
+    expect(dialogElements).toHaveLength(2);
+    fireEvent.click(getByText('Close All'));
+    expect(queryByRole('alertdialog')).not.toBeInTheDocument();
   });
 });
