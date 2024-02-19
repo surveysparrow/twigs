610c949159c003e71184a97ee8482acc026eef76
Akshay U <akshay.uthaman@surveysparrow.com>
2024-02-19 14:25:52 +0530

    Add disabled styles for radio and checkbox

--- packages/react-components/src/checkbox/checkbox.tsx
+++ packages/react-components/src/checkbox/checkbox.tsx
@@ -7,40 +7,16 @@ import { Flex } from '../flex';
 
 const TickIcon = () => {
   return (
-    <svg
-      width="10"
-      height="8"
-      viewBox="0 0 10 8"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M9 1.25L3.5 6.75L1 4.25"
-        stroke="white"
-        strokeWidth="1.8"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+    <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 1.25L3.5 6.75L1 4.25" stroke="white" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   );
 };
 
 const HorizontalLineIcon = () => {
   return (
-    <svg
-      width="10"
-      height="2"
-      viewBox="0 0 10 2"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M9 1H1"
-        stroke="white"
-        strokeWidth="1.8"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+    <svg width="10" height="2" viewBox="0 0 10 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 1H1" stroke="white" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   );
 };
@@ -54,13 +30,17 @@ const StyledCheckbox = styled(CheckboxPrimitive.Root, {
   justifyContent: 'center',
   border: '$borderWidths$xs solid $neutral700',
   transition: 'all $transitions$2',
-  '&:hover': { border: '$borderWidths$xs solid $neutral700' },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+    opacity: 0.4
+  },
+  '&:hover:not(:disabled)': { border: '$borderWidths$xs solid $neutral700' },
   '&:focus-visible': {
     outline: 'none',
     $$shadowColor: '$colors$primary300',
     border: '$borderWidths$xs solid $neutral700',
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 2px, $$shadowColor 0px 0px 0px 4px, rgba(0, 0, 0, 0.05) 0px 1px 2px 0px'
+    boxShadow: 'rgb(255, 255, 255) 0px 0px 0px 2px, $$shadowColor 0px 0px 0px 4px, rgba(0, 0, 0, 0.05) 0px 1px 2px 0px'
   },
   '&:active, &[data-state="checked"], &[data-state="indeterminate"]': {
     background: '$secondary500',
@@ -109,13 +89,13 @@ const StyledLabelContainer = styled('label', {
 type OmitProps = 'onCheckedChange' | 'onChange';
 
 export type CheckboxBaseProps = {
-  checked?: boolean | 'indeterminate'
-  children?: ReactNode,
+  checked?: boolean | 'indeterminate';
+  children?: ReactNode;
   // eslint-disable-next-line no-unused-vars
-  onChange?: (checked: boolean | 'indeterminate') => void,
-  required?: boolean,
-  disabled?: boolean
-}
+  onChange?: (checked: boolean | 'indeterminate') => void;
+  required?: boolean;
+  disabled?: boolean;
+};
 
 export type CheckboxProps = CheckboxBaseProps &
   Omit<ComponentProps<typeof StyledCheckbox>, OmitProps> &
@@ -125,30 +105,14 @@ export type CheckboxProps = CheckboxBaseProps &
   };
 
 export const Checkbox: FunctionComponent<CheckboxProps> = ({
-  checked,
-  disabled,
-  required,
-  onChange,
-  children,
-  id,
-  ...rest
+  checked, disabled, required, onChange, children, id, ...rest
 }) => {
   const isIndeterminate = checked === 'indeterminate';
   const uniqueId = id || useId();
   return (
     <Flex alignItems="center">
-      <StyledCheckbox
-        checked={checked}
-        onCheckedChange={onChange}
-        disabled={disabled}
-        required={required}
-        id={uniqueId}
-        {...(isIndeterminate && { 'data-state': 'indeterminate' })}
-        {...rest}
-      >
-        <StyledIndicator
-          {...(isIndeterminate && { 'data-state': 'indeterminate' })}
-        >
+      <StyledCheckbox checked={checked} onCheckedChange={onChange} disabled={disabled} required={required} id={uniqueId} {...(isIndeterminate && { 'data-state': 'indeterminate' })} {...rest}>
+        <StyledIndicator {...(isIndeterminate && { 'data-state': 'indeterminate' })}>
           <StyledCheckIconContainer className="check-icon">
             <TickIcon />
           </StyledCheckIconContainer>
