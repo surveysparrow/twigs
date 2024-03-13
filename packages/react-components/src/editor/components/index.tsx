7e9254ccfa835aa205575c4a560d6f0837f46918
Akshay U <akshay.uthaman@surveysparrow.com>
2024-03-13 18:09:17 +0530

    Export moved components from directory

--- packages/react-components/src/editor/components/index.tsx
+++ packages/react-components/src/editor/components/index.tsx
@@ -1,1 +1,6 @@
-export * from './editor-area';
+export * from './base-dropdown';
+export * from './floating-toolbar';
+export * from './dialog-link-editor';
+export * from './rich-editor';
+export * from './toolbar';
+export * from './toolbar-context';
