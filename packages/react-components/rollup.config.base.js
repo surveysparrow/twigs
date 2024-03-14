d0efc5877cc576cb945b86e6db80924052bb9aa1
Akshay U <akshay.uthaman@surveysparrow.com>
2024-03-14 18:19:43 +0530

    Use replace plugin to replace import meta env with process env

--- packages/react-components/rollup.config.base.js
+++ packages/react-components/rollup.config.base.js
@@ -6,6 +6,7 @@ const esbuild = require('rollup-plugin-esbuild').default;
 const alias = require('@rollup/plugin-alias');
 const path = require('path');
 const fs = require('fs');
+const replace = require('@rollup/plugin-replace');
 const packageJson = require('./package.json');
 
 const projectRootDir = path.resolve(__dirname);
@@ -35,6 +36,11 @@ module.exports = {
   external: [...Object.keys(packageJson.peerDependencies)],
   plugins: [
     external(),
+    replace({
+      'import.meta.env': 'process.env',
+      'import.meta.env.MODE': 'process.env.NODE_ENV',
+      preventAssignment: false
+    }),
     resolve(),
     commonjs(),
     alias({
