705e2e36459c0af0c988bdd45c13c3d2ae5c03b3
Pratosh <pradosh.balaji@gmail.com>
2024-05-15 13:34:40 +0530

    Refactor Separator component styling and usage in React components

--- packages/react-components/src/separator/separator.tsx
+++ packages/react-components/src/separator/separator.tsx
@@ -3,7 +3,7 @@ import * as SeparatorPrimitive from '@radix-ui/react-separator';
 import { CSS } from '@stitches/react';
 import { styled, config } from '../stitches.config';
 
-const StyledSeparator = styled(SeparatorPrimitive.Root, {
+const SeparatorRoot = styled(SeparatorPrimitive.Root, {
   backgroundColor: '$neutral100',
   '&[data-orientation=horizontal]': { height: 1, width: '100%' },
   '&[data-orientation=vertical]': { height: '100%', width: 1 }
@@ -19,7 +19,7 @@ export type SeparatorProps = {
 export const Separator = forwardRef<HTMLDivElement, SeparatorProps>(
   ({ orientation, decorative, ...props }, ref) => {
     return (
-      <StyledSeparator
+      <SeparatorRoot
         ref={ref}
         data-testid="separator"
         orientation={orientation}
--- packages/react-components/src/separator/stories/separator.stories.tsx
+++ packages/react-components/src/separator/stories/separator.stories.tsx
@@ -1,10 +1,10 @@
-import { Separator as SeparatorRoot, SeparatorProps } from '../separator';
+import { Separator ,SeparatorProps } from '../separator';
 import { Box } from '../../box';
 import { Text } from '../../text';
 import { Flex } from '../../flex';
 
 export default {
-  component: SeparatorRoot,
+  component: Separator,
   title: 'Form/Separator'
 };
 
@@ -12,7 +12,7 @@ const Template = ({ orientation = 'vertical', ...args }: SeparatorProps) => (
   <Box css={{ width: '100%', maxWidth: 300, margin: '0 15px' }}>
     <Flex css={{ height: 20, alignItems: 'center' }}>
       <Text>Blog</Text>
-      <SeparatorRoot
+      <Separator
         orientation={orientation}
         {...args}
         css={{
@@ -20,7 +20,7 @@ const Template = ({ orientation = 'vertical', ...args }: SeparatorProps) => (
         }}
       />
       <Text>Docs</Text>
-      <SeparatorRoot
+      <Separator
         orientation={orientation}
         {...args}
         css={{
