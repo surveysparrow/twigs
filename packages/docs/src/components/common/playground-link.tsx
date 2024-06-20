c66df0ba2b77f42fe0de9595881b241eaad3f04a
Akshay U <akshay.uthaman@surveysparrow.com>
2024-06-20 18:42:56 +0530

    Added playground link component

---
+++ packages/docs/src/components/common/playground-link.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { Box } from "@sparrowengg/twigs-react";
+
+export const PlaygroundLink = ({
+  name,
+  title = "Try in playground ",
+  block = false,
+}: {
+  name: string;
+  title?: string;
+  block?: boolean;
+}) => {
+  const {
+    siteConfig: { customFields },
+  } = useDocusaurusContext();
+
+  const anchorTag = (
+    <a
+      href={`${customFields.playgroundURL}/snippets?name=${name}`}
+      target="_blank"
+    >
+      {title}
+    </a>
+  );
+
+  if (block) {
+    return (
+      <Box
+        css={{
+          margin: "$2 0",
+        }}
+      >
+        {anchorTag}
+      </Box>
+    );
+  }
+
+  return anchorTag;
+};
