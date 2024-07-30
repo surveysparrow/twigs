05b344f4eb35c665959612ab946d5451b8db539a
Maneesh P <maneesh@SSLAP0219.local>
2024-07-30 17:01:54 +0530

    updated border radius of alert based on size, corrected spacing between text and icons in alert, updated line height for alert variants, updated story code with AlertDescription

--- packages/docs/src/theme/ReactLiveScope/index.js
+++ packages/docs/src/theme/ReactLiveScope/index.js
@@ -13,7 +13,8 @@ import {
   RichEditor,
   HashTagNode,
   HashTagPlugin,
-  EditorLookupDropdownBase
+  EditorLookupDropdownBase,
+  AlertDescription
 } from "@sparrowengg/twigs-react";
 
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
@@ -62,6 +63,7 @@ const ReactLiveScope = {
   DatePicker: twigsComponentLoader("DatePicker"),
   CalendarRange: twigsComponentLoader("CalendarRange"),
   Alert: twigsComponentLoader("Alert"),
+  AlertDescription: twigsComponentLoader("AlertDescription"),
   Toastr: twigsComponentLoader("Toastr"),
   toast: twigsComponentLoader("toast"),
   ToastAction: twigsComponentLoader("ToastAction"),
--- packages/react-components/src/alert/alert.tsx
+++ packages/react-components/src/alert/alert.tsx
@@ -30,7 +30,6 @@ const StyledAlert = styled(Box, {
   width: '100%',
   borderWidth: '$xs',
   borderStyle: 'solid',
-  borderRadius: '$lg',
   [`${StyledAlertDescription}`]: {
     flex: 1,
     minWidth: 0
@@ -39,28 +38,34 @@ const StyledAlert = styled(Box, {
     size: {
       sm: {
         padding: '$3 $4',
+        borderRadius: '$lg',
         [`${StyledAlertIcon}`]: {
           marginInlineEnd: '$2'
         },
         [`${StyledAlertDescription}`]: {
-          fontSize: '$sm'
+          fontSize: '$sm',
+          lineHeight: '$sm'
         },
         [`${StyledCloseButton}`]: {
           height: '$5',
-          width: '$5'
+          width: '$5',
+          marginInlineStart: '$2'
         }
       },
       md: {
         padding: '$8',
+        borderRadius: '$xl',
         [`${StyledAlertIcon}`]: {
           marginInlineEnd: '$4'
         },
         [`${StyledAlertDescription}`]: {
-          fontSize: '$md'
+          fontSize: '$md',
+          lineHeight: '$md'
         },
         [`${StyledCloseButton}`]: {
           height: '$6',
-          width: '$6'
+          width: '$6',
+          marginInlineStart: '$4'
         }
       }
     },
@@ -112,7 +117,7 @@ export type AlertProps = {
 export const Alert: FunctionComponent<AlertProps> = React.forwardRef(
   ({
     closable,
-    size,
+    size = 'sm',
     onClose,
     status = 'info',
     children,
