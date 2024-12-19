b48644e3d3c23b3ce8b10cf3506f5d50fae83c95
Akshay U <akshay.uthaman@surveysparrow.com>
2024-12-19 15:03:57 +0530

    Add ref to image component

--- packages/react-components/src/image/image.tsx
+++ packages/react-components/src/image/image.tsx
@@ -1,6 +1,6 @@
 import { prefixClassName } from '@src/utils';
 import clsx from 'clsx';
-import { ComponentProps } from 'react';
+import { ComponentProps, forwardRef } from 'react';
 import { styled } from '../stitches.config';
 
 const StyledImage = styled('img');
@@ -11,11 +11,14 @@ export type ImageProps = Omit<StyledImageProps, 'alt'> & {
   alt: string;
 };
 
-export const Image = (props: ImageProps) => {
-  return (
-    <StyledImage
-      {...props}
-      className={clsx(prefixClassName('image'), props.className)}
-    />
-  );
-};
+export const Image = forwardRef<HTMLImageElement, ImageProps>(
+  (props: ImageProps, ref) => {
+    return (
+      <StyledImage
+        ref={ref}
+        {...props}
+        className={clsx(prefixClassName('image'), props.className)}
+      />
+    );
+  }
+);
