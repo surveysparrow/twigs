712b00d8fd79e1ec0c3a07c851d608d2725ec57d
Akshay U <akshay.uthaman@surveysparrow.com>
2025-01-17 14:03:24 +0530

    Add delayDuration and disableHoverableContent prop to Tooltip root

--- packages/react-components/src/tooltip/tooltip.tsx
+++ packages/react-components/src/tooltip/tooltip.tsx
@@ -96,6 +96,8 @@ export const Tooltip = ({
   size,
   open,
   defaultOpen,
+  delayDuration,
+  disableHoverableContent,
   onOpenChange,
   ...props
 }: TooltipProps) => {
@@ -104,6 +106,8 @@ export const Tooltip = ({
       open={open}
       defaultOpen={defaultOpen}
       onOpenChange={onOpenChange}
+      delayDuration={delayDuration}
+      disableHoverableContent={disableHoverableContent}
     >
       <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
       {content ? (
