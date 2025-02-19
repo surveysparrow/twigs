af89e2e039a06371a4edf697d1a436fe4b6838a4
Justin George <justin.george@surveysparrow.com>
2025-02-19 18:52:33 +0530

    eslint fix

--- packages/react-components/src/stitches.config.ts
+++ packages/react-components/src/stitches.config.ts
@@ -366,6 +366,6 @@ export function hexToRgba(hex: string, opacity: number) {
   return `#${((1 << 24) + (r << 16) + (g << 8) + b)
     .toString(16)
     .slice(1)}${Math.round(opacity * 255)
-      .toString(16)
-      .padStart(2, '0')}`;
+    .toString(16)
+    .padStart(2, '0')}`;
 }
