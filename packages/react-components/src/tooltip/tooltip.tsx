e87db9e7b3fb26ef9cc90388b3e4c0572aafec00
Justin George <justin.george@surveysparrow.com>
2025-09-01 10:25:54 +0530

    updated y offset slide for tooltip

--- packages/react-components/src/tooltip/tooltip.tsx
+++ packages/react-components/src/tooltip/tooltip.tsx
@@ -3,22 +3,22 @@ import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import { styled, keyframes } from '../stitches.config';
 
 const slideUpAndFade = keyframes({
-  '0%': { opacity: 0, transform: 'translateY(2px)' },
-  '100%': { opacity: 1, transform: 'translateY(0)' }
-});
-
-const slideRightAndFade = keyframes({
-  '0%': { opacity: 0, transform: 'translateX(-2px)' },
-  '100%': { opacity: 1, transform: 'translateX(0)' }
-});
-
-const slideDownAndFade = keyframes({
   '0%': { opacity: 0, transform: 'translateY(-2px)' },
   '100%': { opacity: 1, transform: 'translateY(0)' }
 });
 
-const slideLeftAndFade = keyframes({
+const slideRightAndFade = keyframes({
   '0%': { opacity: 0, transform: 'translateX(2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' }
+});
+
+const slideDownAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' }
+});
+
+const slideLeftAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(-2px)' },
   '100%': { opacity: 1, transform: 'translateX(0)' }
 });
 
