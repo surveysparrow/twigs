9db3b92f5b5cdfad3e55d6f831d1e555a7eab791
Akshay U <akshay.uthaman@surveysparrow.com>
2025-09-09 16:53:30 +0530

    Add focus trap

--- packages/react-components/src/drawer/drawer.tsx
+++ packages/react-components/src/drawer/drawer.tsx
@@ -202,6 +202,24 @@ export const Drawer = ({
         ) {
           handleClose();
         }
+        if (e.key === 'Tab') {
+          const focusableElements = portalRef.current?.querySelectorAll(
+            'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+          ) ?? [];
+
+          const firstElement = focusableElements[0] as HTMLElement;
+          const lastElement = focusableElements[
+            focusableElements.length - 1
+          ] as HTMLElement;
+
+          if (e.shiftKey && document.activeElement === firstElement) {
+            e.preventDefault();
+            lastElement.focus();
+          } else if (!e.shiftKey && document.activeElement === lastElement) {
+            e.preventDefault();
+            firstElement.focus();
+          }
+        }
       }}
     >
       <StyledDrawerContainer>
