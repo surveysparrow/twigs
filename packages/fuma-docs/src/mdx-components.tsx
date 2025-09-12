1fd75824c4fbab279a222934fdc52b9d0f5813d5
Gautham Srinivasan <gauthamsrinivasan21@gmail.com>
2025-09-12 17:10:44 +0530

    changed the external link approach

--- packages/fuma-docs/src/mdx-components.tsx
+++ packages/fuma-docs/src/mdx-components.tsx
@@ -3,6 +3,7 @@ import * as TabsComponents from "fumadocs-ui/components/tabs";
 import type { MDXComponents } from "mdx/types";
 import { Playground } from "./component/playground/playground";
 import * as TypeTableComponents from "fumadocs-ui/components/type-table";
+import { ExternalLinks } from "./component/external-link";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
@@ -12,5 +13,6 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...TypeTableComponents,
     ...components,
     Playground,
+    ExternalLinks,
   };
 }
